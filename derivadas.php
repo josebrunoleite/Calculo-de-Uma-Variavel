b995d0ee68c841019ad7f5bf8e121fc350816c2e
Umarekawaru <haikaraumarekawaru@gmail.com>
2022-12-05 10:37:01 -0300

    Ligação de Programação
    Inserindo o código já feito até o dia 05/12/2022

---
+++ derivadas.php
@@ -0,0 +1,24 @@
+<?php
+include_once('padrao/header.php');
+include_once('padrao/navbar.php');
+?>
+<body>
+    <!--<h1>Titulo 01</h1>
+    <h2>Titulo 02</h2>
+    <h3>Titulo 03</h3>
+    <h4>Titulo 04</h4>
+    <p>Paragrafo</p>
+    <img src="img/index/02.jpg" width="200px" alt="">-->
+    <div id="Texto" class="d-flex text-center text-wrap flex-column">
+
+    <h1>Conceito de uma derivada</h1>
+    <h3>A derivada nada mais é: O coeficiente angular da reta tangente a f no ponto de abscissa x0.</h3>
+    <p>Derivadas na Engenharia: No ramo da engenharia o uso das derivadas é bastante frequente, 
+        nas construções de grande porte a derivada é usada para calcular por exemplo, o dimensionamento de vigas,
+        para construção de estruturas hidráulicas,<br> topográficas e etc. As derivadas sua usada para cálculos de esforços 
+        de estruturas, cálculos de momento fletor. </p>
+    </div>
+</body>
+<?php
+include_once('padrao/footer.php');
+?>
---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+include_once('padrao/header.php');
+include_once('padrao/navbar.php');
+?>
+
+<body>
+    <!--Info
+<div class="card mb-3">
+  <img src="img/index/01.jpg" class="card-img-top img-thumbnail ali" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+</div>
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+  <img src="img/index/02.jpg" class="card-img-bottom img-thumbnail ali" alt="...">
+</div>-->
+
+<!--Info 2-->
+<div class="card text-bg-dark mt-2 container">
+  <img src="img/index/01.jpg" class="card-img ali" alt="...">
+  <div class="card-img-overlay text-end">
+    <h5 class="card-title">Titulo</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small>Author</small></p>
+  </div>
+</div>
+<!--Acordeon-->
+<div class="container-xxl mt-5">
+<div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Accordion Item #1
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Accordion Item #2
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Accordion Item #3
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+    
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingFour">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseThree">
+        Accordion Item #4
+      </button>
+    </h2>
+    <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+    
+  </div>
+</div>
+</div>
+</body>
+
+</html>
